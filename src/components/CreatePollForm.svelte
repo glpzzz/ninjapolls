e214b3125b3c8e610db0477e529fc87e2b33c1c6
Gabriel A. López López <glpz@daxslab.com>
2022-07-10 01:44:55 -0400

    add the logic for form validation

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -7,9 +7,45 @@
         answerA: '',
         answerB: ''
     }
+    let errors = {
+        question: '',
+        answerA: '',
+        answerB: ''
+    }
+    let valid = false;
 
     const submitHandler = () => {
-        console.log(fields);
+        valid = true;
+        // validate question
+        if (fields.question.trim().length < 5) {
+            valid = false;
+            errors.question = 'Question must be at least 5 chars long.';
+        } else {
+            errors.question = '';
+        }
+
+        // validate answer A
+        if (fields.answerA.trim().length < 1) {
+            valid = false;
+            errors.answerA = 'Answer A can\'t be empty';
+        } else {
+            errors.answerA = '';
+        }
+
+        // validate answer B
+        if (fields.answerB.trim().length < 1) {
+            valid = false;
+            errors.answerB = 'Answer B can\'t be empty';
+        } else {
+            errors.answerB = '';
+        }
+
+        if (valid) {
+            console.log('valid', fields);
+        } else {
+            console.log(errors);
+        }
+
     }
 </script>
 
@@ -18,16 +54,19 @@
     <div class="form-field">
         <label for="question">Poll Question:</label>
         <input type="text" id="question" bind:value={fields.question}>
+        <p class="errors">{errors.question}</p>
     </div>
 
     <div class="form-field">
         <label for="answer-a">Answer A:</label>
         <input type="text" id="answer-a" bind:value={fields.answerA}>
+        <p class="errors">{errors.answerA}</p>
     </div>
 
     <div class="form-field">
         <label for="answer-b">Answer B:</label>
         <input type="text" id="answer-b" bind:value={fields.answerB}>
+        <p class="errors">{errors.answerB}</p>
     </div>
 
     <Button type="secondary" flat="{true}">Add Poll</Button>
@@ -54,6 +93,13 @@
     label {
         margin: 10px auto;
         text-align: left;
+    }
+
+    .errors{
+        font-size: 85%;
+        color: #d91b42;
+        font-weight: bold;
+
     }
 
 </style>
