34fbbd17179885a1df80c87ee96a57b75c3f7ece
Gabriel A. López López <glpz@daxslab.com>
2022-07-10 00:46:10 -0400

    Add header and footer components

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,21 @@
 <script>
+
+    import Header from './components/Header.svelte';
+    import Footer from './components/Footer.svelte';
+
 </script>
 
+<Header/>
 <main>
-	<h1>Hello ninjas!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium error illo magnam molestias, porro quasi
+        rerum? Accusantium adipisci commodi doloremque esse ex explicabo harum molestias perspiciatis! Dolor dolores
+        modi nisi?</p>
 </main>
+<Footer />
 
-<style></style>
+<style>
+    main {
+        max-width: 960px;
+        margin: 40px auto;
+    }
+</style>
